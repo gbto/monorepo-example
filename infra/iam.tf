054312a576e5ceb8ae6adca43596df0ca6697f5c
Quentin Gaborit <gibboneto@gmail.com>
2022-07-04 20:57:22 +0200

    feat: Add OpenID Connect for GitHub Actions connection to AWS

---
+++ infra/iam.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+  ]
+}
+
+data "aws_iam_policy_document" "github" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:gbto/pants-default-lock:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github" {
+  name               = "github-oidc"
+  assume_role_policy = data.aws_iam_policy_document.github.json
+}
