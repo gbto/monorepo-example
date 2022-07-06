8eb6a12d94716bdb488d32ac1eea40c86aba991a
Quentin Gaborit <gibboneto@gmail.com>
2022-07-06 12:11:48 +0200

    chore: Make python scripts flake8 compliant

--- jobs/job_a/main.py
+++ jobs/job_a/main.py
@@ -1,3 +1,4 @@
+"""Example of a job a."""
 import click
 
 from utils.package_1.main import action_1
@@ -5,9 +6,11 @@ from utils.package_2.main import action_2
 
 
 @click.command()
-def display():
+def display() -> None:
+    """Display result of actions from other modules."""
     click.echo(action_1())
     click.echo(action_2())
+    print("Finished")
 
 
 if __name__ == "__main__":
--- jobs/job_b/main.py
+++ jobs/job_b/main.py
@@ -1,10 +1,12 @@
+"""Example of a job b."""
 import click
 
 from utils.package_1.main import action_1
 
 
 @click.command()
-def display():
+def display() -> None:
+    """Display result of actions from other modules."""
     click.echo(action_1())
     print("Finished")
 
