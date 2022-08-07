d1a5fb5727cf274c6076266c0fadbf41a208d091
Quentin Gaborit <gibboneto@gmail.com>
2022-08-07 22:17:00 +0200

    feat: Add example of packages modules yaml config imports

--- utils/package_1/main.py
+++ utils/package_1/main.py
@@ -1,10 +1,19 @@
 """A utils example for pants build testing purposes."""
-import pandas as pd
+import yaml
+import os
 
 
 def action_1() -> None:
     """An example action to execute within job_a."""
-    df = pd.DataFrame()
-    print(df)
     print("Doing stuff from package 1")
+
+    if os.environ.get("PEX"):
+        config = yaml.safe_load(open("utils/package_1/config.yaml"))
+    else:
+        folder_path = os.path.dirname(__file__)
+        config_path = os.path.join(folder_path, "config.yaml")
+        config = yaml.safe_load(open(config_path))
+
+    print("Like printing config of package_1:")
+    print(config)
     return
